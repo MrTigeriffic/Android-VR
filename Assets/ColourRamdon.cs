275f9f7f0f6d2285a326183ddd5d873556b1f5bc
Oran Chadwick <oranchad@gmail.com>
2021-02-18 17:28:56 +0000

    First Commit
    Builds but not currently loading to current android device

---
+++ Assets/ColourRamdon.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourRamdon : MonoBehaviour
+{
+
+    public void RandomColour()
+    {
+        GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+    }
+
+    public void Red()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+}
